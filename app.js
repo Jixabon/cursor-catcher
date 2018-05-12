fe71665f7b224249863d9796feb752344143cae3
Jixabon <sherlock656@gmail.com>
2018-05-11 23:11:13 -0500

    added socket static dir

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ server.listen(config.server.port, config.server.ip_address, () => {
 });
 
 app.use(express.static('public'));
+app.use(express.static('node_modules/socket.io-client/dist'));
 
 var Game = require('./game/Game');
 
