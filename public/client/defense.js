a803a244990515775cabeeb00cbd3123136f14f3
Jixabon <sherlock656@gmail.com>
2018-05-12 16:11:42 -0500

    Changed client namespace path

--- public/client/defense.js
+++ public/client/defense.js
@@ -1,4 +1,4 @@
-var socket = io('http://nodejs-1.jixabon.com');
+var socket = io('http://cursor-catcher-cursor-catcher.7e14.starter-us-west-2.openshiftapps.com');
 
 $(document).ready(function() {
     socket.emit('client_join', {username: "sherlock"});
