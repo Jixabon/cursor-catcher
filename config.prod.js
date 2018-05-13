ca5ca4d98387b573dcb355ac1190f1cfd4a4b6ac
Jixabon <sherlock656@gmail.com>
2018-05-13 10:57:55 -0500

    prod server port 8000

--- config.prod.js
+++ config.prod.js
@@ -2,7 +2,7 @@ var config = {}
         // production environment config
         // server Config
         config.server = {};
-        config.server.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        config.server.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000;
         config.server.ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 module.exports = config;
