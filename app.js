0882134bf7af5f37cf7363ac17410cd4cb7b9913
Jixabon <sherlock656@gmail.com>
2018-05-13 01:37:21 -0500

    Changes to app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var express = require('express');
 
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io', {
+    transports: ['websocket']
+})(server);
 
 app.use(express.static(__dirname + '/public'));
 
