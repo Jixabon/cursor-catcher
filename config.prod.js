03aac3b8359e5dc59ed194899a133d8f85c49255
Jixabon <sherlock656@gmail.com>
2018-05-13 00:38:47 -0500

    And More Changes

--- config.prod.js
+++ config.prod.js
@@ -3,6 +3,6 @@ var config = {}
         // server Config
         config.server = {};
         config.server.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-        config.server.ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+        config.server.ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 module.exports = config;
