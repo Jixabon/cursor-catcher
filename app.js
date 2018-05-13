cc8c6622498cf1273718278baf3a24d2ca400200
Jixabon <sherlock656@gmail.com>
2018-05-13 00:16:07 -0500

    Changes

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-io.configure(function(){
-    io.set("transports", ["websocket"]);
-});
-
 app.use(express.static(__dirname + '/public'));
 app.use('/socket.io/socket.io.js', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.js'));
 
-app.listen(config.server.port, config.server.ip_address, () => {
+server.listen(config.server.port, config.server.ip_address, () => {
     console.log(`Server running at http://${config.server.ip_address}:${config.server.port}/`);
 });
 
--- public/client/defense.js
+++ public/client/defense.js
@@ -1,4 +1,4 @@
-var socket = io('ws://cursor-catcher-cursor-catcher.7e14.starter-us-west-2.openshiftapps.com:8000');
+var socket = io.connect('http://cursor-catcher-cursor-catcher.7e14.starter-us-west-2.openshiftapps.com:8000');
 
 $(document).ready(function() {
     socket.emit('client_join', {username: "sherlock"});
